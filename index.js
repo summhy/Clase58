d944d3c8a885bc45f2b4ba9d798b4a15a0c7794b
Susana Muñoz <su.munozh@gmail.com>
2023-07-18 21:26:19 -0400

    incorporacion api y post

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 //1.-importamos Express
 import express from "express";
+//importar body-parse para lectura del formulario
+import bodyParser from "body-parser";
+
+import axios from "axios";
 //2.-Creamos la constante para la aplicación
 const app =express();
+
+
 const servicios=[{'nombre':'Mochilas', 'precio':10000},
                  {'nombre':'cuadernos', 'precio':1200},
                  {'nombre':'libros','precio':11000},
@@ -9,6 +15,8 @@ const servicios=[{'nombre':'Mochilas', 'precio':10000},
 
 //definición de la carpeta pública en caso de ser necesario
 app.use(express.static("public"))
+//uso de body parse 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //función de mensaje
 function mensaje(){
@@ -37,7 +45,12 @@ app.get("/servicios",(req, res)=>{
     for (let i=0 ;i < servicios.length;i++){
         html += `<li><a href='/servicio/${i}'>${servicios[i].nombre}</a></li>`
     }
-    html +=`</ul>`;
+    html +=`</ul>
+    <form method='post' action='/servicio'>
+    <input name="id" type="text" placeholder="id">
+    <input name="nombre" type="text"  placeholder="nombre">
+    <input value="Buscar" type="submit">
+    </form>`;
 
 
     return res.send(html)
@@ -48,8 +61,24 @@ app.get("/servicio/:id",(req, res)=>{
     //enviar respuesta al cliente con un texto simple
     return res.send(`<h1>Servicio ${servicios[id].nombre} $ ${servicios[id].precio}</h1>`)
 })
+app.post("/servicio",(req, res)=>{
+    console.log(req.body);
+    //console.log(req.params);
+    const id = req.body.id;
+    //enviar respuesta al cliente con un texto simple
+    return res.send(`<h1>Servicio ${servicios[id].nombre} $ ${servicios[id].precio}</h1>`)
+})
 
-
+app.get('/api', async (req, res) => {
+    try {
+        //para obtener key https://www.marvel.com/signin?referer=https%3A%2F%2Fdeveloper.marvel.com%2Faccount
+      const response = await axios.get('https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=xxxxxxxx&hash=xxxxxx');
+      res.send(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error en el servidor');
+    }
+  });
 
 app.all("*",(req, res)=>{
     return res.send(`<h1>Sitio No existe</h1>`)
